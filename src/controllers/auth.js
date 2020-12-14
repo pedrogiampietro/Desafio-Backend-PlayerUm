9ed069458fe998876806b98b2a759f69a39bfbb8
Pedro Giampietro <pedrogiampietro@hotmail.com>
2020-12-14 20:49:38 -0300

    Storing accounts into database moocking.

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,13 +1,26 @@
 const express = require('express')
+const bcrypt = require('bcrypt')
+const { Account } = require('../models')
 
 const router = express.Router()
+const saltRounds = 10
 
 router.post('/sign-in', async (req, res) => {
 	return res.json('Signed in')
 })
 
 router.post('/sign-up', async (req, res) => {
-	return res.json('Signed up')
+	const email = 'pedro@pedro.com'
+	const password = '123456'
+
+	const hash = bcrypt.hashSync(password, saltRounds)
+
+	const result = await Account.create({
+		email,
+		password: hash,
+	})
+
+	return res.json(result)
 })
 
 module.exports = router
