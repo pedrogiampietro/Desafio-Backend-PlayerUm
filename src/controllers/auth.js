6b165b770b90bf74b0103ceb8f50366360acc9fe
Pedro Giampietro <pedrogiampietro@hotmail.com>
2020-12-14 21:49:36 -0300

    Added Joi.

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const bcrypt = require('bcrypt')
 const { Account } = require('../models')
+const { accountSignIn, accountSignUp } = require('../validators/account')
 
 const router = express.Router()
 const saltRounds = 10
 
-router.post('/sign-in', async (req, res) => {
+router.post('/sign-in', accountSignIn, async (req, res) => {
 	return res.json('Signed in')
 })
 
-router.post('/sign-up', async (req, res) => {
+router.post('/sign-up', accountSignUp, async (req, res) => {
 	const { body } = req
 	const { email, password } = body
 
