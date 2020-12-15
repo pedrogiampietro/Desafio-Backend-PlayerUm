07ca88a44cf0b03d79f9b9391c6c6003cd9d3e78
Pedro Giampietro <pedrogiampietro@hotmail.com>
2020-12-14 23:04:07 -0300

    Jwt implemented.

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -3,12 +3,26 @@ const bcrypt = require('bcrypt')
 const { Account } = require('../models')
 const { accountSignIn, accountSignUp } = require('../validators/account')
 const { getMessage } = require('../helpers/messages')
+const { generateJwt, generateRefreshJwt } = require('../helpers/jwt')
 
 const router = express.Router()
 const saltRounds = 10
 
 router.post('/sign-in', accountSignIn, async (req, res) => {
-	return res.json('Signed in')
+	const { email, password } = req.body
+	const account = await Account.findOne({ where: { email } })
+
+	// validar o password; // primeiro parametro é o password digitado o segundo é o que vem do banco.
+	const match = account ? bcrypt.compareSync(password, account.password) : null
+	if (!match) res.jsonBadRequest(getMessage('account.signin.failed'))
+
+	const token = generateJwt({ id: account.id })
+	const refreshToken = generateRefreshJwt({ id: account.id })
+
+	return res.jsonOK(account, getMessage('account.signin.success'), {
+		token,
+		refreshToken,
+	})
 })
 
 router.post('/sign-up', accountSignUp, async (req, res) => {
@@ -26,7 +40,15 @@ router.post('/sign-up', accountSignUp, async (req, res) => {
 		password: hash,
 	})
 
-	return res.jsonOK(newAccount, getMessage('account.signup.success'))
+	const token = generateJwt({ id: newAccount.id })
+	const refreshToken = generateRefreshJwt({ id: newAccount.id })
+
+	return res.jsonOK(newAccount, getMessage('account.signup.success'), {
+		token,
+		refreshToken,
+	})
 })
+
+module.exports = router
 
 module.exports = router
