fa6eba7b3de17d7c3ba50d801d7265acc9ffe7cb
Pedro Giampietro <pedrogiampietro@hotmail.com>
2020-12-15 19:31:17 -0300

    Rename Variables.

--- src/config/config.js
+++ src/config/config.js
@@ -1,27 +1,27 @@
 require('dotenv').config()
 
 module.exports = {
-	development: {
-		username: process.env.LOCAL_USERNAME,
-		password: process.env.LOCAL_PASSWORD,
-		database: process.env.LOCAL_DATABASE,
-		host: process.env.LOCAL_HOST,
-		dialect: 'mysql',
-	},
-	test: {
-		username: 'root',
-		password: null,
-		database: 'database_test',
-		host: '127.0.0.1',
-		dialect: 'mysql',
-		operatorAliases: '0',
-	},
-	production: {
-		username: 'root',
-		password: null,
-		database: 'database_production',
-		host: '127.0.0.1',
-		dialect: 'mysql',
-		operatorAliases: '0',
-	},
+  development: {
+    username: process.env.LOCAL_USERNAME,
+    password: process.env.LOCAL_PASSWORD,
+    database: process.env.LOCAL_DATABASE,
+    host: process.env.LOCAL_HOST,
+    dialect: 'mysql',
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    operatorAliases: '0',
+  },
+  production: {
+    username: 'root',
+    password: null,
+    database: 'database_production',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    operatorAliases: '0',
+  },
 }
