05b4e0d7d57f0f56ba4586b8bdcfc62b7ee92691
Pedro Giampietro <pedrogiampietro@hotmail.com>
2020-12-20 18:11:36 -0300

    CaseSensitive ajust

--- src/models/place.js
+++ src/models/place.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.INTEGER,
         allowNull: false,
         references: {
-          model: 'accounts',
+          model: 'account',
           key: 'id',
         },
       },
